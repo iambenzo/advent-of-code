c9ccb51a5e38949151a5d53ca1c4260e3f58cac2
iAmBenzo <iambenzo@users.noreply.github.com>
2020-12-12 23:23:53 +0000

    day 2

---
+++ 2020/day-02/02.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+func main() {
+    // Add some arg parsing sauce
+    if len(os.Args) <= 1 {
+        fmt.Println("I need an input file")
+        os.Exit(1)
+    }
+
+    // Counters
+    validPasswords1 := 0
+    validPasswords2 := 0
+
+    // Regex FTW!
+    r := regexp.MustCompile(`(\d+)-(\d+) ([a-z]): ([a-z]+)`)
+
+    entries, err := readFile(os.Args[1])
+    checkError(err)
+
+    // For each line, parse the rule
+    // apply the rule, adjust the vaild count
+    for _, line := range entries {
+        parts := strings.Split(line, " ")
+        if len(parts) == 3 {
+            rgx := r.FindStringSubmatch(line)
+            // fmt.Printf("%#v\n", rgx)
+
+            // Convert strings to ints
+            min, err := strconv.Atoi(rgx[1])
+            checkError(err)
+            max, err := strconv.Atoi(rgx[2])
+            checkError(err)
+
+            letterCount := strings.Count(rgx[4], rgx[3])
+
+            // Check against part 1 rules
+            if letterCount >= min && letterCount <= max {
+                validPasswords1++
+            }
+
+            // Check against part 2 rules
+            parsedPassword := []rune(rgx[4])
+            if string(parsedPassword[min-1]) == rgx[3] && string(parsedPassword[max-1]) != rgx[3] {
+                validPasswords2++
+            } else if string(parsedPassword[min-1]) != rgx[3] && string(parsedPassword[max-1]) == rgx[3] {
+                validPasswords2++
+            }
+
+        }
+    }
+
+    fmt.Println(fmt.Sprintf("[Part 1} Number of valid password: %d", validPasswords1))
+    fmt.Println(fmt.Sprintf("[Part 2} Number of valid password: %d", validPasswords2))
+
+}
+
+// Dirty util function for checking errors
+func checkError(e error) {
+    if e != nil {
+        panic(e)
+    }
+}
+
+// Read file contents into a map that's been bastardised
+// into some form of set
+func readFile(file string) ([]string, error) {
+    fileBytes, err := ioutil.ReadFile(file)
+    return strings.Split(string(fileBytes), "\n"), err
+}
